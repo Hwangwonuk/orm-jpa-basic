e502888f97a9df1b7ff4495606666a043c96cc1b
Hwangwonuk <hok0173@naver.com>
2023-01-11 16:57:02 +0900

    실전예제 4 - 상속관계 매핑

--- jpa-shop/src/main/java/io/wonuk/shop/JpaMain.java
+++ jpa-shop/src/main/java/io/wonuk/shop/JpaMain.java
@@ -2,10 +2,12 @@ package io.wonuk.shop;
 
 import io.wonuk.shop.domain.Book;
 
+import io.wonuk.shop.domain.Member;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import org.hibernate.Hibernate;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,11 +20,16 @@ public class JpaMain {
 
         try {
 
-            Book book = new Book();
-            book.setName("자바 표준 ORM 프로그래밍");
-            book.setAuthor("김영한");
+            Member member1 = new Member();
+            member1.setName("member1");
+            em.persist(member1);
 
-            em.persist(book);
+            em.flush();
+            em.clear();
+
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); // Proxy
+            Hibernate.initialize(refMember); // 강제 초기화
 
             tx.commit();
         } catch (Exception e) {
