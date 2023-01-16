0d928f1792c7d5bcb39e61f14b3fdac5751d3905
Hwangwonuk <hok0173@naver.com>
2023-01-16 14:15:56 +0900

    JPQL 타입 표현과 기타식

--- ex2-jpql/src/main/java/jpql/JpaMain.java
+++ ex2-jpql/src/main/java/jpql/JpaMain.java
@@ -1,7 +1,9 @@
 package jpql;
 
+import java.awt.print.Book;
 import java.util.List;
 import jpql.domain.Member;
+import jpql.domain.MemberType;
 import jpql.domain.Team;
 
 import javax.persistence.EntityManager;
@@ -28,17 +30,18 @@ public class JpaMain {
             Member member = new Member();
             member.setName("member1");
             member.setAge(10);
+            member.setMemberType(MemberType.ADMIN);
 
             member.setTeam(team);
 
             em.persist(member);
 
-
             em.flush();
             em.clear();
 
-            String query = "select mm.age, mm.name " + "from (select m.age, m.name from Member m) as mm";
+            String query = "select m.name, 'HELLO', TRUE from Member m where m.memberType = :userType and m.age between 0 and 10";
             List<Member> result = em.createQuery(query, Member.class)
+                .setParameter("userType", MemberType.ADMIN)
                 .getResultList();
 
             System.out.println("result.size() = " + result.size());
