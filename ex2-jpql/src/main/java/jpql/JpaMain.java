894392641f7b3ba78432f096cafbefae4ab883b8
Hwangwonuk <hok0173@naver.com>
2023-01-16 15:56:51 +0900

    페치 조인 1 - 기본

--- ex2-jpql/src/main/java/jpql/JpaMain.java
+++ ex2-jpql/src/main/java/jpql/JpaMain.java
@@ -24,40 +24,58 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setName("member1");
-            member1.setTeam(team);
+            member1.setName("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setName("member2");
-            member2.setTeam(team);
+            member2.setName("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setName("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-//            단일 값 연관 경로 = 묵시적 내부 조인 inner join 발생, 탐색 O
-//            String query = "select m.team from Member m";
-//            String query = "select m.team from Member m";
+//            fetch join을 사용하면 즉시로딩 된다. FetchType.LAZY로 설정되어 있어도 우선적으로 적용된다.
+//            String query = "select m from Member m join fetch m.team";
 
-//            컬렉션 값 연관 경로 - 묵시적 내부 조인 발생, 탐색 X
-//            String query = "select t.members from Team t";
-//            String query = "select t.members.size from Team t";
+//            1:N 조인 관계의 조인에서 결과값이 뻥튀기 될 수 있다.
+//            N:1 조인 관계는 뻥튀기 되지않고 오히려 줄어들기도 한다.
+//            String query = "select t from Team t join fetch t.members";
 
-//            묵시적 조인이 아닌 명시적 조인을 사용하자.
-            String query = "select m.name From Team t join t.members m";
+//            중복 제거 DISTINCT
+//            String query = "select distinct t from Team t join fetch t.members";
 
-            Integer result = em.createQuery(query, Integer.class).getSingleResult();
+//            일반 join 문도 데이터가 뻥튀기 되는것은 동일하다.
+            String query = "select t from Team t join t.members m";
 
-//            for (Integer o : resultList) {
-//                System.out.println("o = " + o);
-//            }
+            List<Team> result = em.createQuery(query, Team.class).getResultList();
 
-            System.out.println("result = " + result);
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + ", members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차캐시)
+                // 회원3, 팀B(SQL)
+
+                // 회원 100명 -> N + 1
+            }
 
             tx.commit();
         } catch (Exception e) {
