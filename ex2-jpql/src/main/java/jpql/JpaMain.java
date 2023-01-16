85c98b406fc6a43e17cda23479bf4772fe185bfb
Hwangwonuk <hok0173@naver.com>
2023-01-16 11:43:27 +0900

    서브쿼리 - JPQL from절 불가

--- ex2-jpql/src/main/java/jpql/JpaMain.java
+++ ex2-jpql/src/main/java/jpql/JpaMain.java
@@ -37,7 +37,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m left join Team t on m.name = t.name";
+            String query = "select mm.age, mm.name " + "from (select m.age, m.name from Member m) as mm";
             List<Member> result = em.createQuery(query, Member.class)
                 .getResultList();
 
