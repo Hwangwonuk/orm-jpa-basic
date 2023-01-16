3e8d6ac9cddf1112a565bd4283a62466987ace51
Hwangwonuk <hok0173@naver.com>
2023-01-16 15:33:18 +0900

    경로 표현식

--- ex2-jpql/src/main/java/jpql/JpaMain.java
+++ ex2-jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import java.awt.print.Book;
+import java.util.Collection;
 import java.util.List;
 import jpql.domain.Member;
 import jpql.domain.MemberType;
@@ -23,30 +24,40 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setName("member1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setName("member2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            JPQL 기본 함수
-//            String query = "select 'a' || 'b' from Member m";
-//            String query = "select locate('de', 'abcdegf') from Member m";
-//            String query = "select index(t.members) from Team t";
+//            단일 값 연관 경로 = 묵시적 내부 조인 inner join 발생, 탐색 O
+//            String query = "select m.team from Member m";
+//            String query = "select m.team from Member m";
 
-//            사용자 정의 함수
-            String query = "select function('group_concat', m.name) from Member m";
-//            hibernate를 사용하면 select group_concat(m.name) from Member m 형태로 사용이 가능하다.
-            List<Integer> resultList = em.createQuery(query, Integer.class).getResultList();
+//            컬렉션 값 연관 경로 - 묵시적 내부 조인 발생, 탐색 X
+//            String query = "select t.members from Team t";
+//            String query = "select t.members.size from Team t";
 
-            for (Integer s : resultList) {
-                System.out.println("s = " + s);
-            }
+//            묵시적 조인이 아닌 명시적 조인을 사용하자.
+            String query = "select m.name From Team t join t.members m";
+
+            Integer result = em.createQuery(query, Integer.class).getSingleResult();
+
+//            for (Integer o : resultList) {
+//                System.out.println("o = " + o);
+//            }
+
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
