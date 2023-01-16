692f9ff59608b63294469e5f5f718c2730d2e899
Hwangwonuk <hok0173@naver.com>
2023-01-16 11:32:37 +0900

    조인

--- ex2-jpql/src/main/java/jpql/JpaMain.java
+++ ex2-jpql/src/main/java/jpql/JpaMain.java
@@ -21,26 +21,27 @@ public class JpaMain {
 
         try {
 
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setName("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.name desc",
-                    Member.class)
-                .setFirstResult(1)
-                .setMaxResults(10)
+            String query = "select m from Member m left join Team t on m.name = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
                 .getResultList();
 
-            System.out.println("result.size = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
+            System.out.println("result.size() = " + result.size());
 
             tx.commit();
         } catch (Exception e) {
